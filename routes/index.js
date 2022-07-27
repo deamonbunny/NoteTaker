41a8a2bfe7425f591b572891eea9cd3f4ad08148
bunny <deamonbunny@gmail.com>
2022-07-26 22:03:28 -0700

    added files from desktop

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const noteRoutes = require("./notes");
+router.use("/notes",noteRoutes);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+const api = require('./routes/notes.js')
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/notes', api)
+
+// const allRoutes = require("./routes")
+// app.use(allRoutes);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
